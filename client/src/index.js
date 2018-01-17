3228bbddea89219c8b5023735afe73a923f325cf
Toucher-F <lianduanb@gmail.com>
2018-01-17 12:48:53 -0500

    add signout page

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import reduxThunk from "redux-thunk";
 
 import App from "./components/app";
 import Signin from "./components/auth/signin";
+import Signout from "./components/auth/signout";
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
@@ -14,8 +15,10 @@ const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
-      <Route path="/" component={App} />
-      <Route path="/signin" component={Signin} />
+      <Route path="/" component={App}>
+        <Route path="/signin" component={Signin} />
+        <Route path="/signout" component={Signout} />
+      </Route>
     </Router>
   </Provider>,
   document.querySelector(".container")
